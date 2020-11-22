1d51f85f804d8bfb3d5f14bb8480b926fc80213c
AlanaFenix <74868860+AlanaFenix@users.noreply.github.com>
2020-11-22 17:20:05 -0300

    Refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,42 @@
-import Discord from 'discord.js';
+import { Client, Message } from 'discord.js'
 import { config } from 'dotenv'
-import databaseConnect from './providers/databaseProvider';
+import databaseConnect from './providers/databaseProvider'
 import blogManager from './structures/BlogManager'
 
-config();
-databaseConnect();
+config()
+databaseConnect()
 
-const client = new Discord.Client();
-
+const client = new Client()
 
 client.on('ready', () => {
     console.log(`[BOT] Online!`)
-});
-
-client.on('message', (message) => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    if(!message.content.startsWith(process.env.prefix!)) return;
-
-    let commandBody = message.content.slice(process.env.PREFIX?.length).split(" ")
-    let commandName = commandBody[0]
-    let args = commandBody.slice(1)
-
-    try {
-        let command = require('./commands/' + commandName)
-        command.default(client, message, args)
-    } catch (error) {
-        message.channel.send(`:x: Este comando não existe.`)
-    }
 })
 
-client.login(process.env.TOKEN).catch((err) => console.log(err));
+client.on('message', listenMessageForCommands)
+
+function listenMessageForCommands(message: Message) {
+    const evaluatedAsCommand = evaluateMessageAsCommand(message)
+
+    if (evaluatedAsCommand) {
+
+        const commandBody = message.content.slice(process.env.PREFIX?.length).split(" ")
+        const commandName = commandBody[0]
+        const args = commandBody.slice(1)
+
+        try {
+            const command = require('./commands/' + commandName)
+            command.default(client, message, args)
+        } catch (error) {
+            message.channel.send(`:x: Este comando não existe.`)
+        }
+    }
+}
+
+function evaluateMessageAsCommand(message: Message) {
+    return !message.author.bot
+        && message.channel.type !== "dm"
+        && message.content
+        && message.content.startsWith(process.env.PREFIX!)
+}
+
+client.login(process.env.TOKEN).catch((err) => console.log(err))
